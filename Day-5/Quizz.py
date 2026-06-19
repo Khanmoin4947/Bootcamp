a52a4b952de8b71f344882339cb50ec5dc8d09bd
Moin Khan <khanmoin4947@gmail.com>
2026-06-19 10:04:31 +0530

    day-6

--- Day-5/Quizz.py
+++ Day-5/Quizz.py
@@ -1,32 +1,34 @@
 def student_report(name, mark):
     print("\n----- Student Report -----")
-    print("Name :", name)
-    print("mark :", mark)
-def add_bonus(mark):
-    mark = mark + 5
-    print("Inside Mark :", mark)
-def sum_mark(n):
-    if n == 1:
-        return 1
-    return n + sum_mark(n - 1)
-def square(x):
-    return x * x
-def cube(x):
-    return x ** 3
-    
+    print(f"Name : {name}")
+    print(f"mark : {mark}")
 
 name = input("Enter Student Name: ")
 mark = int(input("Enter mark: "))
 
 student_report(name, mark)
 
+def add_bonus(mark):
+    mark = mark + 5
+    print(f"Inside Mark : {mark}")
+
 add_bonus(mark)
-print("Outside Function mark :", mark)
+print("Outside mark :", mark)
+
+def sum_mark(n):
+    if n == 1:
+        return 1
+    return n + sum_mark(n - 1)
 
 n = int(input("number for recursive sum: "))
 print("Recursive Sum =", sum_mark(n))
 
+def square(x):
+    return x * x
 
+def cube(x):
+    return x ** 3
+    
 print("Choose Operation:")
 print("1. Square")
 print("2. Cube")
---
+++ Day-6/tryexcept.py
@@ -0,0 +1,4 @@
+x = 4
+
+if not type(x) is int:
+  raise TypeError("Only integers are allowed")
