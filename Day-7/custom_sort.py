2f0a5e46d0cfbb703df29d583e1f56052d86793f
Moin Khan <khanmoin4947@gmail.com>
2026-06-19 11:00:27 +0530

    lamda func practice example

---
+++ Day-7/custom_sort.py
@@ -0,0 +1,12 @@
+dict=[
+    {"moin": 9.8}, 
+    {"farhan": 8.9}, 
+    {"navjot": 9.5}, 
+    {"sahil": 8.2}
+    ]
+
+x=sorted(dict, key=lambda x: list(x.values())[0], reverse=True)
+
+print(x)
+
+
---
+++ Day-7/mini_calc.py
@@ -0,0 +1,25 @@
+def operators(op):
+      if op == '+':
+         return lambda x, y: x + y
+      elif op == '-':
+         return lambda x, y: x - y
+      elif op == '*':
+         return lambda x, y: x * y
+      elif op == '/':
+         return lambda x, y: x / y
+      else:
+         raise ValueError('Invalid operator')
+      
+
+add = operators('+')
+subtract = operators('-')
+multiply = operators('*')
+divide = operators('/')
+
+input1 = float(input("Enter an num1: "))
+input2 = float(input("Enter an num2: "))
+op = input("Enter an operator (+, -, *, /): ")
+
+result = operators(op)
+print(result(input1, input2))
+
