31b652da83de6eb248051e2c687b83f5aa9f1581
Moin Khan <khanmoin4947@gmail.com>
2026-06-23 11:48:54 +0530

    item inventory

---
+++ Day-8_Object-Oriented-Programming/Item-inventory.py
@@ -0,0 +1,29 @@
+class Product():
+    def __init__(self, name,price,quantity):
+        self.name=name
+        self.price=price
+        self.quantity=quantity
+
+    def total_value(self):
+        return self.price*self.quantity
+    
+product_list=[
+    ("Laptop: ", 50000, 2),
+    ("Mouse: ", 500, 10),
+    ("Keyboard: ", 1500, 5)
+]
+products=[]
+
+for name, price, quantity in product_list:
+    product=Product(name, price, quantity)
+    products.append(product)
+
+grand_total = 0
+
+for product in products:
+    value = product.total_value()
+    grand_total += value
+    print(f"{product.name}{value}")
+print("Grand Total Inventory Value:", grand_total)
+
+    
